fb925c320ff019d1f342d87a93e86b4ff7fc8230
ExtaZzZ4106 <ExtaZzZ123456789@yandex.ru>
2024-01-29 17:24:51 +0300

    complited listings3_5

--- Java_listings_Gornostaev/src/main/java/Listing_3/Listing3_1.java
+++ Java_listings_Gornostaev/src/main/java/Listing_3/Listing3_1.java
@@ -1,4 +1,29 @@
 package Listing_3;
 
-public class Listing3_1 {
+class Vihecle3_1 {
+    int passengers;
+    int wheels;
+    int maxspeed;
+    int burnup;
 }
+class VehicleDemo  {
+
+    public static void main(String[] args)  {
+
+        Vihecle3_1 car1 = new Vihecle3_1();
+        car1.passengers = 2;
+        car1.wheels = 6;
+        car1.maxspeed = 130;
+        car1.burnup = 30;
+
+        double distance = car1.maxspeed * 0.5;
+
+        System.out.println("За пол часа car1 может проехать:" + distance + "км");
+
+        car1 = null;
+    }
+
+}
+
+
+
