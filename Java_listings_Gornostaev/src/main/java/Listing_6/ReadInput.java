f5c8f0711d8b8c20122b7b6eb020ba56ce97bbfe
ExtaZzZ4106 <ExtaZzZ123456789@yandex.ru>
2024-02-16 23:12:14 +0300

    fix: complited all

---
+++ Java_listings_Gornostaev/src/main/java/Listing_6/ReadInput.java
@@ -0,0 +1,13 @@
+package Listing_6;
+
+public class ReadInput {
+    public ReadInput(String command) {
+    }
+
+    public boolean readLine() {
+        return false;
+    }
+
+    public void close() {
+    }
+}
